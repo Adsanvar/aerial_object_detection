a41d943c491b419cc7514c55fddfbc3c56038a58
Adrian Sandoval <adriansandovalvargas@gmail.com>
2020-02-28 16:23:22 -0500

    cropper update

--- AerialObjectDetection/cropper.py
+++ AerialObjectDetection/cropper.py
@@ -3,8 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-original = cv2.imread('images/08-07-352-007.png')
+#value of hue: 
+#RGB: 1, 255, 255
+#Hex: 01FFFF
+#HSV: 180, 100, 100
+
+name = '08-07-352-007.png'
+original = cv2.imread('images/'+name)
 cpy = np.copy(original)
+
+#converts the image to transparent png
+def blackToTransparent(img):
+
+    src = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+
+    bgr = src[:,:,:3] # Channels 0..2
+    gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+
+    # Some sort of processing...
+
+    bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    alpha = src[:,:,3] # Channel 3
+    result = np.dstack([bgr, alpha]) # Add the alpha channel
+    cv2.imshow("test?", result)
+    cv2.waitKey(0)
+
+
+    # cv2.imwrite('51IgH_result.png', result)
+
 ##Kmeans image segmentation
 vectorized = cpy.reshape((-1, 3))
 vectorized = np.float32(vectorized)
@@ -26,6 +52,8 @@ cv2.imshow("result_HSV", imghsv)
 cv2.waitKey(0)
 
 ##-----------------------------
+
+##CONTOUR STUFF
 
 ###Contour via grayScale
 #gray image from segmentated image
@@ -50,61 +78,33 @@ for contour in contours:
         cv2.drawContours(cpy, [approx], 0, (0), 1)
         x,y = approx[0][0]
         if inner:
-            cv2.imshow("or", cpy)
+            #Black mask
+            mask = np.zeros_like(cpy)
+            #fills in the mask with the shape of our approximation
+            cv2.fillPoly(mask, [approx], color=(255,255,255))
+            out = np.zeros_like(original)
+            out[mask == 255] = original[mask == 255]
+            cv2.imshow("or", out)
             cv2.waitKey(0)
             #study masks!!!
             #https://stackoverflow.com/questions/28759253/how-to-crop-the-internal-area-of-a-contour
-            #https://bytefish.de/blog/extracting_contours_with_opencv/
-            #rect = cv2.minAreaRect(contour)
-            # box = cv2.boxPoints(rect)
-            # ext_left = tuple(approx[approx[:, :, 0].argmin()][0])
-            # ext_right = tuple(approx[approx[:, :, 0].argmax()][0])
-            # ext_top = tuple(approx[approx[:, :, 1].argmin()][0])
-            # ext_bot = tuple(approx[approx[:, :, 1].argmax()][0])
+
+            #cropping edges
+            ext_left = tuple(approx[approx[:, :, 0].argmin()][0])
+            ext_right = tuple(approx[approx[:, :, 0].argmax()][0])
+            ext_top = tuple(approx[approx[:, :, 1].argmin()][0])
+            ext_bot = tuple(approx[approx[:, :, 1].argmax()][0])
             #roi == approx
-            # roi_corners = np.array([box], dtype=np.int32)
-            # cv2.polylines(img, roi_corners, 1, (255, 0, 0), 3)
-            # cv2.imshow('image', img)
-            # cv2.waitKey(0)
-            # cropped_image = original[ext_top[1]:ext_bot[1], ext_left[0]:ext_right[0]]
+            #Cropping
+            cropped_image = out[ext_top[1]:ext_bot[1], ext_left[0]:ext_right[0]]
             # cv2.imwrite('crop{}.jpg'.format('_'+str(1)), cropped_image)
-            # cropped_image = 
+            # cv2.imwrite('{}'.format(name),cropped_image)
             # cv2.imshow("final?", cropped_image)
             # cv2.waitKey(0)
 
+            
+
         inner = False
-        
-        # rect = cv2.minAreaRect(contour)
-        # box = cv2.boxPoints(rect)
-        # ext_left = tuple(approx[approx[:, :, 0].argmin()][0])
-        # ext_right = tuple(approx[approx[:, :, 0].argmax()][0])
-        # ext_top = tuple(approx[approx[:, :, 1].argmin()][0])
-        # ext_bot = tuple(approx[approx[:, :, 1].argmax()][0])
-        # roi_corners = np.array([box], dtype=np.int32)
-        # cv2.polylines(img, roi_corners, 1, (255, 0, 0), 3)
-        # cv2.imshow('image', img)
-        # cv2.waitKey(0)
-        # cropped_image = original[ext_top[1]:ext_bot[1], ext_left[0]:ext_right[0]]
-        # cv2.imwrite('crop{}.jpg'.format('_'+str(1)), cropped_image)
 
 
-
-
-
-
-##Contour Via HSV
-#value of hue: 
-#RGB: 1, 255, 255
-#Hex: 01FFFF
-#HSV: 180, 100, 100
-# hue = np.array([1,255,255])
-# imghsv = cv2.cvtColor(cpy, cv2.COLOR_BGR2HSV)
-
-# cv2.imshow("HSV", imghsv)
-# cv2.waitKey(0)
-# mask = cv2.inRange(imghsv, hue, hue)
-
-# fimg, contours, hierarchy2 = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# cv2.drawContours(cpy, contours, -1, (255,0,0), 8)
-# cv2.imshow("HSV", cpy)
-# cv2.waitKey(0)
+blackToTransparent(name)
