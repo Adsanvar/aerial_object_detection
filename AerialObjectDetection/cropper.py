7c697aa19f92162063f69dd883f3c4eb693306d8
Adrian <adriansandovalvargas@gmail.com>
2020-02-29 11:12:29 -0500

    adds alpha channel to black mask

--- AerialObjectDetection/cropper.py
+++ AerialObjectDetection/cropper.py
@@ -8,103 +8,89 @@ import os
 #Hex: 01FFFF
 #HSV: 180, 100, 100
 
-name = '08-07-352-007.png'
-original = cv2.imread('images/'+name)
-cpy = np.copy(original)
-
 #converts the image to transparent png
-def blackToTransparent(img):
+def blackToTransparent(name, img):
+    tmp = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+    b, g, r = cv2.split(img)
+    rgba = [b,g,r, alpha]
+    dst = cv2.merge(rgba,4)
+    cv2.imwrite('cropped/{}'.format(name), dst)
 
-    src = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+for file in os.listdir('images'):
 
-    bgr = src[:,:,:3] # Channels 0..2
-    gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+    original = cv2.imread('images/'+file)
+    cpy = np.copy(original)
 
-    # Some sort of processing...
+    ##Kmeans image segmentation
+    vectorized = cpy.reshape((-1, 3))
+    vectorized = np.float32(vectorized)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 4
+    attempts = 10
+    img = cv2.cvtColor(cpy, cv2.COLOR_BGR2RGB)
+    ret, label, center = cv2.kmeans(vectorized, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    #final image
+    result_image = res.reshape((img.shape))
+    #cv2.imshow("result", result_image)
+    #cv2.waitKey(0)
 
-    bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-    alpha = src[:,:,3] # Channel 3
-    result = np.dstack([bgr, alpha]) # Add the alpha channel
-    cv2.imshow("test?", result)
-    cv2.waitKey(0)
+    # need to convert to HSV to obtain a darkline from the blue hue
+    imghsv = cv2.cvtColor(result_image, cv2.COLOR_BGR2HSV)
+    #cv2.imshow("result_HSV", imghsv)
+    #cv2.waitKey(0)
 
+    ##-----------------------------
 
-    # cv2.imwrite('51IgH_result.png', result)
+    ##CONTOUR STUFF
 
-##Kmeans image segmentation
-vectorized = cpy.reshape((-1, 3))
-vectorized = np.float32(vectorized)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 4
-attempts = 10
-img = cv2.cvtColor(cpy, cv2.COLOR_BGR2RGB)
-ret, label, center = cv2.kmeans(vectorized, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
-center = np.uint8(center)
-res = center[label.flatten()]
-#final image
-result_image = res.reshape((img.shape))
-cv2.imshow("result", result_image)
-cv2.waitKey(0)
+    ###Contour via grayScale
+    #gray image from segmentated image
+    gray = cv2.cvtColor(imghsv, cv2.COLOR_RGB2GRAY)
 
-# need to convert to HSV to obtain a darkline from the blue hue
-imghsv = cv2.cvtColor(result_image, cv2.COLOR_BGR2HSV)
-cv2.imshow("result_HSV", imghsv)
-cv2.waitKey(0)
+    #cv2.imshow("gray", gray)
+    #v2.waitKey(0)
 
-##-----------------------------
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
+    #cv2.imshow("tresh", thresh)
+    #cv2.waitKey(0)
 
-##CONTOUR STUFF
+    #Finds the contours
+    img2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-###Contour via grayScale
-#gray image from segmentated image
-gray = cv2.cvtColor(imghsv, cv2.COLOR_RGB2GRAY)
+    #processes the contours and recrops the image to the property size
+    inner = True
+    for contour in contours:
+        if cv2.contourArea(contour) > 4000:
+            epsilon = 0.0001*cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            cv2.drawContours(cpy, [approx], 0, (0), 1)
+            x,y = approx[0][0]
+            if inner:
+                #Black mask
+                mask = np.zeros_like(cpy)
+                #fills in the mask with the shape of our approximation
+                cv2.fillPoly(mask, [approx], color=(255,255,255))
+                out = np.zeros_like(original)
+                out[mask == 255] = original[mask == 255]
+                #cv2.imshow("or", out)
+                #cv2.waitKey(0)
+                #study masks!!!
+                #https://stackoverflow.com/questions/28759253/how-to-crop-the-internal-area-of-a-contour
 
-cv2.imshow("gray", gray)
-cv2.waitKey(0)
+                #cropping edges
+                ext_left = tuple(approx[approx[:, :, 0].argmin()][0])
+                ext_right = tuple(approx[approx[:, :, 0].argmax()][0])
+                ext_top = tuple(approx[approx[:, :, 1].argmin()][0])
+                ext_bot = tuple(approx[approx[:, :, 1].argmax()][0])
+                #roi == approx
+                #Cropping
+                cropped_image = out[ext_top[1]:ext_bot[1], ext_left[0]:ext_right[0]]
+                blackToTransparent(file ,cropped_image)
+                
 
-ret, thresh = cv2.threshold(gray, 127, 255, 0)
-cv2.imshow("tresh", thresh)
-cv2.waitKey(0)
-
-#Finds the contours
-img2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-#processes the contours and recrops the image to the property size
-inner = True
-for contour in contours:
-    if cv2.contourArea(contour) > 4000:
-        epsilon = 0.0001*cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, epsilon, True)
-        cv2.drawContours(cpy, [approx], 0, (0), 1)
-        x,y = approx[0][0]
-        if inner:
-            #Black mask
-            mask = np.zeros_like(cpy)
-            #fills in the mask with the shape of our approximation
-            cv2.fillPoly(mask, [approx], color=(255,255,255))
-            out = np.zeros_like(original)
-            out[mask == 255] = original[mask == 255]
-            cv2.imshow("or", out)
-            cv2.waitKey(0)
-            #study masks!!!
-            #https://stackoverflow.com/questions/28759253/how-to-crop-the-internal-area-of-a-contour
-
-            #cropping edges
-            ext_left = tuple(approx[approx[:, :, 0].argmin()][0])
-            ext_right = tuple(approx[approx[:, :, 0].argmax()][0])
-            ext_top = tuple(approx[approx[:, :, 1].argmin()][0])
-            ext_bot = tuple(approx[approx[:, :, 1].argmax()][0])
-            #roi == approx
-            #Cropping
-            cropped_image = out[ext_top[1]:ext_bot[1], ext_left[0]:ext_right[0]]
-            # cv2.imwrite('crop{}.jpg'.format('_'+str(1)), cropped_image)
-            # cv2.imwrite('{}'.format(name),cropped_image)
-            # cv2.imshow("final?", cropped_image)
-            # cv2.waitKey(0)
-
-            
-
-        inner = False
+            inner = False
 
 
-blackToTransparent(name)
